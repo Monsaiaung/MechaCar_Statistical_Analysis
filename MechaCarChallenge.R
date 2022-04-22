e752c8c3306c31ac80a05765bc2be19a73e5094e
Mon <monsaiaung.mon@gmail.com>
2022-04-21 22:30:44 -0400

    add

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,3 +17,13 @@ total_summary <- Suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI)
 
 lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = "keep")
 
+#Deliverable 3
+
+t.test(Suspension_coil$PSI,mu= 1500) #Across all Lots
+
+t.test(subset(Suspension_coil,Manufacturing_Lot=="Lot1")$PSI,mu=1500) #Lot1
+
+t.test(subset(Suspension_coil,Manufacturing_Lot=="Lot2")$PSI,mu=1500) #Lot2
+
+t.test(subset(Suspension_coil,Manufacturing_Lot=="Lot3")$PSI,mu=1500) #Lot3
+
